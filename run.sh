d5f632ebda6334cf62927c6403beccb51f7f5dc5
Brendan Hansen <brendan.f.hansen@gmail.com>
2017-10-18 11:48:15 -0500

    make it work, i think...

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-pulp server --host $IP --port $PORT --no-psa
+pulp -w build --to www/main.js -O -j 8 --no-psa --skip-entry-point
--- src/Main.purs
+++ src/Main.purs
@@ -1,17 +1,23 @@
 module Main where
 
+import Graphics.Canvas
 import Prelude
+
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-
 import Data.Maybe (Maybe(..))
-
-import Graphics.Canvas
-
 import Partial.Unsafe (unsafePartial)
 
+getCanvas :: forall e. String -> Eff (canvas :: CANVAS | e) CanvasElement
+getCanvas name = unsafePartial $ do
+    Just canvas <- getCanvasElementById name
+    pure canvas
+
 main :: forall e. Eff _ Unit
-main = unsafePartial $ do
-    Just canvas <- getCanvasElementById "game"
-    
+main = do
+    canvas <- getCanvas "game"
+    ctx <- getContext2D canvas
+
+    _ <- setFillStyle "black" ctx
+    _ <- fillRect ctx { x : 0.0, y : 0.0, w : 1000.0, h : 1000.0 }
     pure unit
